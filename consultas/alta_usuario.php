582be3a19419f102ee0e171046ba3e31fd9bb32e
parinisantiago <parinisantiago@gmail.com>
2016-05-22 19:18:12 -0300

    agregada validacion del lado del servidor del alta de usuario

---
+++ consultas/alta_usuario.php
@@ -0,0 +1,26 @@
+<?php
+    include_once("../conectarBD.php");
+
+    $email= $_POST['emailUser'];
+
+    $query= "SELECT email FROM usuario WHERE email='".$email."'";
+    $resultado= mysqli_query($conexion, $query);
+
+    if(mysqli_num_rows($resultado) == 0){
+
+        $nombre=$_POST['nomUser'];
+        $apellido=$_POST['apUser'];
+        $clave=$_POST['passUser'];
+        $fNac=$_POST['fNacUser'];
+        $tel=$_POST['numUser'];
+        
+        $insert="INSERT INTO usuario(email,nombre, apellido, fnac, telefono, clave) VALUES('".$email."','".$nombre."','".$apellido."','".$fNac."','".$tel."','".$clave."')";
+
+        mysqli_query($conexion,$insert);
+        header("Location: ../index.php?msg=Su usuario se ha creado correctamente&&class=alert-success");
+
+    } else {
+
+        header("Location: ../user_form.php?msg=Ya existe un usuario que posee ese nombre mail&&class=alert-danger");
+  
+    }
