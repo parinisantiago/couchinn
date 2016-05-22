d0af1a68ecfdf47fb0604bfa5b61d612487ddc3c
parinisantiago <parinisantiago@gmail.com>
2016-05-22 20:50:06 -0300

    modificado error en redireccion

--- listado_tipos_hospedajes.php
+++ listado_tipos_hospedajes.php
@@ -57,5 +57,5 @@ if( isset($_SESSION['sesion_usuario']) ){
 <?php
 	//si no esta loggueado redirige al usuario al index y le manda por get el mensaje correspondiente y setea al error como un warning
 	} else {
-		header("Location: index.php/msg=debe estar logueado para ver esta pagina&&class=alert-warning");
+		header("Location: index.php?msg=debe estar logueado para ver esta pagina&&class=alert-warning");
 }?>
