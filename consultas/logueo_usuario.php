19217b7cec469091bff3a7abc1073427d1fb88e4
parinisantiago <parinisantiago@gmail.com>
2016-05-22 21:32:10 -0300

    el logueo ahora setea una session especial para el usuario

--- consultas/logueo_usuario.php
+++ consultas/logueo_usuario.php
@@ -8,6 +8,10 @@
     $query= "SELECT id_usuario,email,clave FROM usuario WHERE email='".$email."' AND clave='".$clave."'";
     $resultado=mysqli_query($conexion, $query);
 
+
+    $queryAdmin= "SELECT * FROM admin WHERE email='".$email."' AND clave='".$clave."'";
+    $resultadoAdmin=mysqli_query($conexion, $query);
+
     if (mysqli_num_rows($resultado) == 1) {
         session_start();
         $row= mysqli_fetch_array($resultado);
@@ -15,9 +19,24 @@
         $_SESSION["id_usuario"]= $row['id_usuario'];
         $class="alert-success";
         $msg="Se ha iniciado sesión correctamente";
-    } else {
-        $class="alert-danger";
-        $msg="<strong>Error al iniciar sesión</strong>. Usuario o contraseña incorrecta";
-    }
 
-    header("Location: ../index.php?class=".$class."&&msg=".$msg);
+    // si no es usuario comun se fija si es admin
+    } else {
+        $queryAdmin = "SELECT * FROM admin WHERE email='". $email ."' AND clave='".$clave."'";
+        $resultadoAdmin = mysqli_query($conexion, $queryAdmin);
+        if (mysqli_num_rows($resultadoAdmin) == 1) {
+            session_start();
+            $rowAdmin = mysqli_fetch_array($resultadoAdmin);
+            $_SESSION["sesion_usuario"] = true;
+            $_SESSION["id_usuario"] = $rowAdmin['id_admin'];
+            $_SESSION['admin'] = true;
+            $class = "alert-success";
+            $msg = "Se ha iniciado sesión correctamente como administrador";
+
+        //si no es ninguno de los dos tira error
+        } else {
+            $class = "alert-danger";
+            $msg = "<strong>Error al iniciar sesión</strong>. Usuario o contraseña incorrecta";
+        }
+    }
+  header("Location: ../index.php?class=".$class."&&msg=".$msg);
