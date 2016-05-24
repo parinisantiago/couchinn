2650ec33f08847296ddb436ad1c77a3dd20e373b
parinisantiago <parinisantiago@gmail.com>
2016-05-24 09:07:07 -0300

    corregi un error en las consultas

--- consultas/modificar_tipo_hospedaje.php
+++ consultas/modificar_tipo_hospedaje.php
@@ -4,13 +4,14 @@
 
     $nombre= $_POST["nomTipo"];
 
-    $query= "SELECT nombre FROM tipo WHERE id_tipo=".$_POST['id_tipo']."";
+    $query= "SELECT nombre FROM tipo WHERE id_tipo='".$_POST['idTipo']."'";
+    echo($query);
     $resultado= mysqli_query($conexion, $query);
     $row=mysqli_fetch_array($resultado);
 
     if($row == $nombre) {
 
-       $update="UPDATE tipo SET nombre='".$nombre."' WHERE id_tipo=".$_POST['id_tipo']."";
+       $update="UPDATE tipo SET nombre='".$nombre."' WHERE id_tipo='".$_POST['idTipo']."'";
         echo($update);
         mysqli_query($conexion,$update);
         header("Location: ../listado_tipos_hospedajes.php?msg=Su tipo se ha modificado correctamente&&class=alert-success");
@@ -19,14 +20,14 @@
         $resultado= mysqli_query($conexion, $query);
         if (mysqli_num_rows($resultado) == 0) {
 
-            $update="UPDATE tipo SET nombre='".$nombre."' WHERE id_tipo=".$_POST['id_tipo']."";
+            $update="UPDATE tipo SET nombre='".$nombre."' WHERE id_tipo='".$_POST['idTipo']."'";
             echo($update);
             mysqli_query($conexion, $update);
             header("Location: ../listado_tipos_hospedajes.php?msg=Su tipo se ha modificado correctamente&&class=alert-success");
 
         } else {
 
-            header("Location: modificar_tipo_hospedaje.php?msg=Ya existe un tipo que posee ese nombre&&class=alert-danger");
+           header("Location: modificar_tipo_hospedaje.php?msg=Ya existe un tipo que posee ese nombre&&class=alert-danger");
 
         }
     }
