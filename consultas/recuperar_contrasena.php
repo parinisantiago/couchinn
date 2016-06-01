d778596b4859c2d39a3af910771ddf6d66f51c4f
parinisantiago <parinisantiago@gmail.com>
2016-06-01 20:25:15 -0300

    agregado mensaje de success

--- consultas/recuperar_contrasena.php
+++ consultas/recuperar_contrasena.php
@@ -11,9 +11,9 @@
     if($row["email"] == $email) {
         echo "<script>
         alert('Se ha enviado a su mail: ".$email." la siguiente contraseña: ".$row["clave"]."');
-        window.location.href='../index.php';
+        window.location.href='../index.php?msg=Se ha enviado su contraseña a su dirección de correo exitosamente&&class=alert-success';
         </script>";
     }else {
-        header("Location: ../user_recuperarContrasena_form.php?msg=No existe un email que posee ese nombre&&class=alert-danger");
+        header("Location: ../index.php?msg=No existe un email que posee ese nombre&&class=alert-danger");
     }
     
