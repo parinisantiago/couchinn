8290d72b86ed6c0cf8469e3adcd2c29d10e300c8
parinisantiago <parinisantiago@gmail.com>
2016-06-10 09:44:35 -0300

    ya anda la carga de fotos, falta el resto

--- altaCouch.php
+++ altaCouch.php
@@ -1,131 +1,128 @@
 <?php
-    session_start();
-    if(isset($_SESSION['sesion_usuario'])) {
-        include_once("conectarBD.php");
-        $query= "SELECT id_tipo,nombre_tipo FROM tipo WHERE eliminado = 0";
-        $result=mysqli_query($conexion, $query);
-        ?>
-        <html>
-        <head>
+session_start();
+//if(isset($_SESSION['sesion_usuario'])) {
+include_once("conectarBD.php");
+$query= "SELECT id_tipo,nombre_tipo FROM tipo WHERE eliminado = 0";
+$result=mysqli_query($conexion, $query);
+?>
+<!DOCTYPE html>
+<html>
+<head>
 
-            <meta charset="utf-8">
-            <meta http-equiv="X-UA-Compatible" content="IE=edge">
-            <meta name="viewport" content="width=device-width, initial-scale=1">
-            <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
-            <link href="default.css" rel="stylesheet">
-            <link rel="icon" href="img/logo.png">
-            <script src="js/jquery.min.js"></script>
-            <script src="bootstrap/js/bootstrap.min.js"></script>
-            <script src="js/altaValidaciones.js"></script>
-            <title> Couchinn </title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="default.css" rel="stylesheet">
+    <link rel="icon" href="img/logo.png">
+    <script src="js/jquery.min.js"></script>
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+    <script src="js/altaValidaciones.js"></script>
+    <title> Couchinn </title>
 
-        </head>
+</head>
+<body>
+<?php include("navbar.php") ?>
+<!-- hay un bardo con el formulario de iniciar sesion en la navbar, valida el html cuando no debería. -->
+<div class="container">
+    <?php if (isset($_GET['msg'])) { ?>
+        <div id="alert" role="alert" class="col-md-offset-2 col-md-8 alert <?php echo($_GET['class']) ?>">
+            <?php echo($_GET['msg']) ?>
+        </div>
+    <?php } ?>
+    <form class="form-horizontal" name="altaCouch" method="post" action="upload.php" enctype="multipart/form-data">
+        <div class="form-group">
+            <span class="text-muted"><em><span style="color:red;">*</span> Estos campos son requeridos</em></span>
+        </div>
 
-    <body>
+        <!-- id dueno couch -->
+        <div class="form-group">
+            <label class="control-label" for="idUser"></label>
+            <input type="hidden" name="idUser" class="form-control" id="idUser" value=<?php echo($_SESSION['id_usuario'])?>>
+        </div>
 
-        <?php include("navbar.php") ?>
-        <!-- hay un bardo con el formulario de iniciar sesion en la navbar, valida el html cuando no debería. -->
-    <div class="container">
-        <?php if (isset($_GET['msg'])) { ?>
-            <div id="alert" role="alert" class="col-md-offset-2 col-md-8 alert <?php echo($_GET['class']) ?>">
-                <?php echo($_GET['msg']) ?>
-            </div>
-        <?php } ?>
-        <form class="form-horizontal" name="altaCouch" method="post" onsubmit="return valCouch()" action="consultas/alta_couch.php" enctype="multipart/form-data">
-            <div class="form-group">
-                <span class="text-muted"><em><span style="color:red;">*</span> Estos campos son requeridos</em></span>
-            </div>
+        <!-- titulo couch -->
+        <div class="form-group">
+            <label class="control-label" for="titCouch">Título<span style="color:red;">*</span></label>
+            <input type="text" name="titCouch" class="form-control" id="titCouch" placeholder="El nombre de mi couch" onkeypress="return isLetterKey(event)" maxlength="100" aria-describedby="helpBlock-nom" required>
+            <span id="glyphicon-titCouch" aria-hidden="true"></span>
+            <span id="helpBlock-titCouch" class="help-block"></span>
+        </div>
 
-            <!-- id dueno couch -->
-            <div class="form-group">
-                <label class="control-label" for="idUser"></label>
-                <input type="hidden" name="idUser" class="form-control" id="idUser" value=<?php echo($_SESSION['id_usuario'])?>>
-            </div>
+        <!-- descripcion couch -->
+        <div class="form-group">
+            <label class="control-label" for="descCouch">Descripción<span style="color:red;">*</span></label>
+            <textarea name="descCouch" class="form-control" id="descCouch" maxlength="500" aria-describedby="helpBlock-nom" rows="5" cols="50" required> </textarea>
+            <span id="glyphicon-descCouch" aria-hidden="true"></span>
+            <span id="helpBlock-descCouch" class="help-block"></span>
+        </div>
 
-            <!-- titulo couch -->
-            <div class="form-group">
-                <label class="control-label" for="titCouch">Título<span style="color:red;">*</span></label>
-                <input type="text" name="titCouch" class="form-control" id="titCouch" placeholder="El nombre de mi couch" onkeypress="return isLetterKey(event)" maxlength="100" aria-describedby="helpBlock-nom" required>
-                <span id="glyphicon-titCouch" aria-hidden="true"></span>
-                <span id="helpBlock-titCouch" class="help-block"></span>
-            </div>
+        <!-- ubicacion couch -->
+        <div class="form-group">
+            <label class="control-label" for="ubCouch">Ubicación<span style="color:red;">*</span></label>
+            <input type="text" name="ubCouch" class="form-control" id="ubCouch" placeholder="La Plata, Buenos aires, Argentina" onkeypress="return isLetterKey(event)" maxlength="100" aria-describedby="helpBlock-nom" required>
+            <span id="glyphicon-ubCouch" aria-hidden="true"></span>
+            <span id="helpBlock-ubCouch" class="help-block"></span>
+        </div>
 
-            <!-- descripcion couch -->
-            <div class="form-group">
-                <label class="control-label" for="descCouch">Descripción<span style="color:red;">*</span></label>
-                <textarea name="descCouch" class="form-control" id="descCouch" maxlength="500" aria-describedby="helpBlock-nom" rows="5" cols="50" required> </textarea>
-                <span id="glyphicon-descCouch" aria-hidden="true"></span>
-                <span id="helpBlock-descCouch" class="help-block"></span>
-            </div>
+        <!-- direccion couch -->
+        <div class="form-group">
+            <label class="control-label" for="dirCouch">Dirección<span style="color:red;">*</span></label>
+            <input type="text" name="dirCouch" class="form-control" id="dirCouch" placeholder="Santa Fe e/ Corrientes y Brasil n 1500" maxlength="100" aria-describedby="helpBlock-nom" required>
+            <span id="glyphicon-dirCouch" aria-hidden="true"></span>
+            <span id="helpBlock-dirCouch" class="help-block"></span>
+        </div>
 
-            <!-- ubicacion couch -->
-            <div class="form-group">
-                <label class="control-label" for="ubCouch">Ubicación<span style="color:red;">*</span></label>
-                <input type="text" name="ubCouch" class="form-control" id="ubCouch" placeholder="La Plata, Buenos aires, Argentina" onkeypress="return isLetterKey(event)" maxlength="100" aria-describedby="helpBlock-nom" required>
-                <span id="glyphicon-ubCouch" aria-hidden="true"></span>
-                <span id="helpBlock-ubCouch" class="help-block"></span>
-            </div>
+        <!-- capacidad couch -->
+        <div class="form-group">
+            <label class="control-label" for="capCouch">Capacidad<span style="color:red;">*</span></label>
+            <select class="form-control" id="capCouch" name="capCouch">
+                <?php for($i=1; $i <= 20; $i++){ ?>
 
-            <!-- direccion couch -->
-            <div class="form-group">
-                <label class="control-label" for="dirCouch">Dirección<span style="color:red;">*</span></label>
-                <input type="text" name="dirCouch" class="form-control" id="dirCouch" placeholder="Santa Fe e/ Corrientes y Brasil n 1500" maxlength="100" aria-describedby="helpBlock-nom" required>
-                <span id="glyphicon-dirCouch" aria-hidden="true"></span>
-                <span id="helpBlock-dirCouch" class="help-block"></span>
-            </div>
+                    <option value=<?php echo($i); ?>> <?php echo($i); ?> </option>
 
-            <!-- capacidad couch -->
-            <div class="form-group">
-                <label class="control-label" for="capCouch">Capacidad<span style="color:red;">*</span></label>
-                <select class="form-control" id="dirCouch" name="dirCouch">
-                    <?php for($i=1; $i <= 20; $i++){ ?>
+                <?php } ?>
+            </select>
+        </div>
 
-                        <option value=<?php echo($i); ?>> <?php echo($i); ?> </option>
+        <!-- tipo couch -->
+        <div class="form-group">
+            <label class="control-label" for="tipCouch">Tipo de couch<span style="color:red;">*</span></label>
+            <select class="form-control" id="tipCouch" name="tipCouch">
+                <?php while ( $tipos = mysqli_fetch_array($result) ){ ?>
 
-                    <?php } ?>
-                </select>
-            </div>
+                    <option value=<?php echo($tipos['id_tipo']); ?>> <?php echo($tipos['nombre_tipo']); ?> </option>
 
-            <!-- tipo couch -->
-            <div class="form-group">
-                <label class="control-label" for="tipCouch">Tipo de couch<span style="color:red;">*</span></label>
-                <select class="form-control" id="tipCouch" name="tipCouch">
-                    <?php while ( $tipos = mysqli_fetch_array($result) ){ ?>
+                <?php } ?>
+            </select>
+        </div>
 
-                        <option value=<?php echo($tipos['id_tipo']); ?>> <?php echo($tipos['nombre_tipo']); ?> </option>
+        <!-- imagen couch -->
+        <div class="form-group">
+            <label class="control-label" for="imgCouch">Fotos del couch<span style="...">*</span> </label>
+            <input type="file" name="imgCouch" id="imgCouch">
+        </div>
 
-                    <?php } ?>
-                </select>
-            </div>
-
-            <!-- imagen couch -->
-            <div class="form-group">
-                <label class="control-label" for="imgCouch">Fotos del couch<span style="...">*</span> </label>
-                <input type="file" name="imgCouch" id="ímgCouch">
-            </div>
-            
-            <!-- imagen de portada -->
-           
-            <?php
-                $query_premium="SELECT id_usuario FROM premium WHERE id_usuario='". $_SESSION['id_usuario'] ."'";
-                $result_premium= mysqli_query($conexion, $query_premium);
-                if (mysqli_num_rows($result_premium) > 0){ ?>
-                    <div class="form-group">
-                        <label class="control-label" for="imgPrinCouch">Foto principal del couch<span style="...">*</span> </label>
-                        <input type="file" name="imgPrinCouch" id="imgPrinCouch">
-                    </div>
-              <?php  }
-            ?>
-            
-            <!-- botones de envio -->
-            <div class="form-group">
-                <button type="submit" class="btn btn-default">Aceptar</button>
-                <a class="btn btn-default" href="index.php">Cancelar</a>
-            </div>
-        </form>
-    </div>
+        <!-- imagen de portada -->
 
         <?php
-    }else{
-        header("Location: index.php?msg=debe estar logueado para ver esta pagina&&class=alert-warning");
-    }
+        $query_premium="SELECT id_usuario FROM premium WHERE id_usuario='". $_SESSION['id_usuario'] ."'";
+        $result_premium= mysqli_query($conexion, $query_premium);
+        if (mysqli_num_rows($result_premium) > 0){ ?>
+            <div class="form-group">
+                <label class="control-label" for="imgPrinCouch">Foto principal del couch<span style="...">*</span> </label>
+                <input type="file" name="imgPrinCouch" id="imgPrinCouch">
+            </div>
+        <?php  }
+        ?>
+
+        <!-- botones de envio -->
+        <div class="form-group">
+            <button type="submit" class="btn btn-default" name="submit">Aceptar</button>
+            <a class="btn btn-default" href="index.php">Cancelar</a>
+        </div>
+    </form>
+</div>
+
+</body>
+</html>
--- consultas/alta_couch.php
+++ consultas/alta_couch.php
@@ -1,23 +1,42 @@
 <?php
-    include_once("../conectarBD.php");
+include_once("conectarBD.php");
 
-    //datos enviados por POST
-    $idUser= $_POST['idUser'];
-    $titCouch= $_POST['titCouch'];
-    $descCouch= $_POST['descCouch'];
-    $ubCouh= $_POST['ubCouch'];
-    $dirCouch= $_POST['dirCouch'];
-    $capCouch= $_POST['capCouch'];
-    $tipCouch= $_POST['tipCouch'];
+//datos enviados por POST
+$idUser= $_POST['idUser'];
+$titCouch= $_POST['titCouch'];
+$descCouch= $_POST['descCouch'];
+$ubCouh= $_POST['ubCouch'];
+$dirCouch= $_POST['dirCouch'];
+$capCouch= $_POST['capCouch'];
+$tipCouch= $_POST['tipCouch'];
 
-    //direccion base para los couch, se crea una carpeta para cada usuario
-    $baseDir="img/".$idUser;
+//direccion base para los couch, se crea una carpeta para cada usuario
+$dirBase = "fotos_hospedajes/";
+$dirCompleta = $dirBase . basename($_FILES["imgCouch"]["name"]);
+$dirCompletaPrin=$dirBase . basename($_FILES['imgPrinCouch']['name']);
 
-    //validaciones de la imagen del couch
-    $imgCouchDir=$baseDir . basename($_FILES['imgCouch']['name']);
-    $imgCouchType= pathinfo($imgCouchDir, PATHINFO_EXTENSIONH);
 
-    //valida se es una imagen
+//valida que sea es una imagen
+$tipoImgCouch = $_FILES['imgCouch']['type'];
+$tipoImgPrin = $_FILES['imgPrinCouch']['type'];
+if(! ( $tipoImgCouch == 'image/png' || $tipoImgCouch == 'image/jpg' || $tipoImgCouch == 'image/jpeg')){ header("redirect: ../altaCouch.php?variable=entro por el check");}
+if(! ( $tipoImgPrinCouch == 'image/png' || $tipoImgPrinCouch == 'image/jpg' || $tipoImgPrinCouch == 'image/jpeg')){ header("redirect: ../altaCouch.php?variable=entro por el check");}
 
-    $check = getimagesize($_FILES["imgCouch"]["tmp_name"]);
-    if($check !== true) header("redirect: ../altaCouch.php?variable=algo");
+//valida que la imagen no exista
+if (file_exists($dirCompleta)){ echo("algo"); }
+if (file_exists($dirCompletaPrin)){ echo("algo"); }
+
+
+//si pasa todas las validaciones intenta agrega la imagen
+if (move_uploaded_file($_FILES["imgCouch"]["tmp_name"], $dirCompleta)) {
+    echo "The file ". basename( $_FILES["imgCouch"]["name"]). " has been uploaded.";
+} else {
+    echo "Sorry, there was an error uploading your file.";
+}
+
+if (move_uploaded_file($_FILES["imgPrinCouch"]["tmp_name"], $dirCompletaPrin)) {
+    echo "The file ". basename( $_FILES["imgPrinCouch"]["name"]). " has been uploaded.";
+} else {
+    echo "Sorry, there was an error uploading your file.";
+}
+
