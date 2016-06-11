a02d3e2aabd8d522a2d13ec0b54e66734bfbf7cc
leoarmen <leoa72@fibertel.com.ar>
2016-06-10 21:50:36 -0300

    modificaciones en reservas
    Ahora cuando se acepta una reserva, se rechazan las que abarcan la fecha aceptada.

--- consultas/aceptar_rechazar_reserva.php
+++ consultas/aceptar_rechazar_reserva.php
@@ -4,12 +4,22 @@
     
 	if (isset($_POST["aceptar"])){
 
-        $queryAceptarReserva="UPDATE reserva SET estado='Aceptada' WHERE id_reserva='".$_POST["aceptar"]."'";
+        $queryDecisionReserva="UPDATE reserva SET estado='Aceptada' WHERE id_reserva='".$_POST["aceptar"]."'";
+        mysqli_query($conexion, $queryDecisionReserva);
+        $querySeleccionarReserva= "SELECT finicio, ffin FROM reserva WHERE id_reserva='".$_POST["aceptar"]."'";
+        //Cuando se acepta una reserva, se deben rechazar las que estan espera y abarcan la fecha aceptada.
+        $resSeleccionarReserva = mysqli_query($conexion, $querySeleccionarReserva);
+        while ($res = mysqli_fetch_array($resSeleccionarReserva)){
+            $queryRechazarLasDemas="UPDATE reserva SET estado='Rechazada' WHERE (('".$res["finicio"]."' BETWEEN finicio AND fFin) OR ('".$res["ffin"]."' BETWEEN finicio AND ffin) OR (finicio BETWEEN '".$res["finicio"]."' AND '".$res["ffin"]."') OR (ffin BETWEEN '".$res["finicio"]."' AND '".$res["ffin"]."')) AND (id_couch='".$_POST["idCouch"]."') AND (estado = 'En espera')";
+            mysqli_query($conexion, $queryRechazarLasDemas);
+        }
+    
     } 
     else{
 
-        $queryAceptarReserva="UPDATE reserva SET estado='Rechazada' WHERE id_reserva='".$_POST["rechazar"]."'";
+        $queryDecisionReserva="UPDATE reserva SET estado='Rechazada' WHERE id_reserva='".$_POST["rechazar"]."'";
+        mysqli_query($conexion, $queryDecisionReserva);
     }  
-    mysqli_query($conexion, $queryAceptarReserva);
+    
     header("Location: ../detalle_couch.php?id=".$_POST['idCouch']);
 ?>
