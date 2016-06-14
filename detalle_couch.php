5e92ed4b09fba30b0b523c94f0c79af931256d6d
parinisantiago <parinisantiago@gmail.com>
2016-06-14 19:12:55 -0300

    formulario para preguntas

--- detalle_couch.php
+++ detalle_couch.php
@@ -50,123 +50,124 @@ if (mysqli_num_rows($resultado) == 1){
     <?php while ( $couch = mysqli_fetch_array($resultado)) {
 
         //busca las fotos del couch para agregarlas al carousel
-
         $query_foto="SELECT ruta FROM foto WHERE id_couch='".$_GET["id"]."'";
         $resultado_foto=mysqli_query($conexion, $query_foto);
         $cant_fotos=mysqli_num_rows($resultado_foto);
         ?>
 
-        <div id="myCarousel" class="carousel slide" data-ride="carousel">
-            <!-- Indicators -->
-            <ol class="carousel-indicators">
 
-                <?php for($i = 0; $i < $cant_fotos; $i++){ ?>
 
-                    <li data-target="#myCarousel" data-slide-to=<?php echo($i); if($i == 0){ echo(" class=active");} ?>></li>
+                                <div id="myCarousel" class="carousel slide" data-ride="carousel">
+                                    <!-- Indicators -->
+                                    <ol class="carousel-indicators">
 
-                <?php } ?>
+                                        <?php for($i = 0; $i < $cant_fotos; $i++){ ?>
 
-            </ol>
+                                            <li data-target="#myCarousel" data-slide-to=<?php echo($i); if($i == 0){ echo(" class=active");} ?>></li>
 
-            <?php if ($cant_fotos != 0){ ?>
-            <!-- Wrapper for slides class="img-responsive center-block"-->
-                <div class="carousel-inner" role="listbox">
-                    <?php while ( $foto = mysqli_fetch_array($resultado_foto)) {
-                        if( $first){ $first=false;?>
-                        <div class="item active">
-                            <img  src=<?php echo("fotos_hospedajes/".$foto["ruta"]);?> >
-                         </div>
-                    <?php } else {?>
-                        <div class="item">
-                            <img  src=<?php echo("fotos_hospedajes/".$foto["ruta"]);?> >
-                        </div>
-                    <?php }
-                    } ?>
+                                        <?php } ?>
 
-                </div>
-            <?php } else { ?>
-                <div class="carousel-inner" role="listbox">
-                    <div class="item active">
-                        <img  src=<?php echo("img/logo.png");?> >
-                    </div>
-            </div>    
-            <?php }        ?>
-            <!-- Controls -->
-            <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-                <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-                <span class="sr-only">Previous</span>
-            </a>
-            <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-                <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-                <span class="sr-only">Next</span>
-            </a>
-        </div>
+                                    </ol>
 
-        <p class="h2 col-md-offset-4"> Datos del Couch</p>
+                                    <?php if ($cant_fotos != 0){ ?>
+                                        <!-- Wrapper for slides class="img-responsive center-block"-->
+                                        <div class="carousel-inner" role="listbox">
+                                            <?php while ( $foto = mysqli_fetch_array($resultado_foto)) {
+                                                if( $first){ $first=false;?>
+                                                    <div class="item active">
+                                                        <img  src=<?php echo("fotos_hospedajes/".$foto["ruta"]);?> >
+                                                    </div>
+                                                <?php } else {?>
+                                                    <div class="item">
+                                                        <img  src=<?php echo("fotos_hospedajes/".$foto["ruta"]);?> >
+                                                    </div>
+                                                <?php }
+                                            } ?>
 
-        <!-- infromacion del couch -->
+                                        </div>
+                                    <?php } else { ?>
+                                        <div class="carousel-inner" role="listbox">
+                                            <div class="item active">
+                                                <img  src=<?php echo("img/logo.png");?> >
+                                            </div>
+                                        </div>
+                                    <?php }        ?>
+                                    <!-- Controls -->
+                                    <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+                                        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                                        <span class="sr-only">Previous</span>
+                                    </a>
+                                    <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+                                        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                                        <span class="sr-only">Next</span>
+                                    </a>
+                                </div>
 
-        <div class="col-md-offset-2 col-md-6 inf_couch">
-            <d1 class="dl-horizontal">
-                <dt> Título: </dt>
-                <dd> <?php echo($couch["titulo"]) ?></dd>
-                <dt> Descripción: </dt>
-                <dd> <?php echo($couch["descripcion"]) ?></dd>
-                <dt> Tipo:</dt>
-                <dd> <?php echo($couch["nombre_tipo"]) ?></dd>
-                <dt> Ubicación: </dt>
-                <dd> <?php echo($couch["ubicacion"]) ?></dd>
-                <dt> Dirección: </dt>
-                <dd> <?php echo($couch["direccion"]) ?></dd>
-                <dt> Capacidad: </dt>
-                <dd> <?php echo($couch["capacidad"]) ?></dd>
-            </d1>
-        </div>
+                                <p class="h2 col-md-offset-4"> Datos del Couch</p>
 
-        <p class="h2 col-md-offset-4"> Datos del usuario</p>
+                                <!-- infromacion del couch -->
 
-        <!-- informacion del usuario, ni idea porque no se lista igual que el couch -->
+                                <div class="col-md-offset-2 col-md-6 inf_couch">
+                                    <d1 class="dl-horizontal">
+                                        <dt> Título: </dt>
+                                        <dd> <?php echo($couch["titulo"]) ?></dd>
+                                        <dt> Descripción: </dt>
+                                        <dd> <?php echo($couch["descripcion"]) ?></dd>
+                                        <dt> Tipo:</dt>
+                                        <dd> <?php echo($couch["nombre_tipo"]) ?></dd>
+                                        <dt> Ubicación: </dt>
+                                        <dd> <?php echo($couch["ubicacion"]) ?></dd>
+                                        <dt> Dirección: </dt>
+                                        <dd> <?php echo($couch["direccion"]) ?></dd>
+                                        <dt> Capacidad: </dt>
+                                        <dd> <?php echo($couch["capacidad"]) ?></dd>
+                                    </d1>
+                                </div>
 
-        <div class="col-md-offset-2 col-md-6 inf_couch">
-            <d1 class="dl-horizontal">
-                <dt> Nombre: </dt>
-                <dd> <?php echo($couch["nombre"]) ?> <?php echo($couch["apellido"]) ?></dd>
-            </d1>
-        </div>
-        
-    <?php   }    ?>
-    <?php   if (isset($_SESSION["admin"]) && !$esDuenio && !$_SESSION["admin"]) {  ?>
-                <?php include("reservar_couch.php");?>
-                <a class="btn btn-default" href="#" data-toggle="modal" data-target="#modalReservarCouch"> Reservar</a>
-                
-        <?php   }  ?>
-        <?php if($esDuenio){  ?>
-            <!-- Inicio de aceptar o rechazar reservas -->
-            <div class="col-md-7">
-             
-                <div class="panel panel-primary">
-                
-                        <div class="panel-heading">
-                            Listado de reservas en espera por este couch.
-                        </div>
-                            <?php 
-                                include_once("conectarBD.php");
-                                //Se fija cuales de las reservas estan pasadas de fecha para ponerlas como vencidas.
-                                $queryReservasVencidas = "UPDATE reserva SET estado = 'Vencida' WHERE (finicio < CURDATE())";
-                                mysqli_query($conexion, $queryReservasVencidas);
-                                //Ltsta todas las reservas que están en espera de ser aceptadas o rechazadas
-                                $queryReservasEnEspera = "SELECT * FROM reserva NATURAL JOIN usuario WHERE ((id_couch = '".$_GET["id"]."') AND (estado = 'En espera'))";
-                                $resultadoReservasEnEspera = mysqli_query($conexion, $queryReservasEnEspera);
-                            ?>    
-                            <form name="formularioReservasEnEspera" action="consultas/aceptar_rechazar_reserva.php" method="POST">
-                            <?php    while ($rowReservas = mysqli_fetch_array($resultadoReservasEnEspera)) { 
-                            ?>
+                                <p class="h2 col-md-offset-4"> Datos del usuario</p>
 
-                            
-                            <input type="hidden" name="idCouch" class="form-control" id="idCouch" value="<?php echo($_GET["id"])?>">
-                          
-                           
-                           <div class="form-group" id="">
+                                <!-- informacion del usuario, ni idea porque no se lista igual que el couch -->
+
+                                <div class="col-md-offset-2 col-md-6 inf_couch">
+                                    <d1 class="dl-horizontal">
+                                        <dt> Nombre: </dt>
+                                        <dd> <?php echo($couch["nombre"]) ?> <?php echo($couch["apellido"]) ?></dd>
+                                    </d1>
+                                </div>
+
+                            <?php   }    ?>
+                                <?php   if (isset($_SESSION["admin"]) && !$esDuenio && !$_SESSION["admin"]) {  ?>
+                                    <?php include("reservar_couch.php");?>
+                                    <a class="btn btn-default" href="#" data-toggle="modal" data-target="#modalReservarCouch"> Reservar</a>
+
+                                <?php   }  ?>
+                                <?php if($esDuenio){  ?>
+                                    <!-- Inicio de aceptar o rechazar reservas -->
+                                    <div class="col-md-7">
+
+                                        <div class="panel panel-primary">
+
+                                            <div class="panel-heading">
+                                                Listado de reservas en espera por este couch.
+                                            </div>
+                                            <?php
+                                            include_once("conectarBD.php");
+                                            //Se fija cuales de las reservas estan pasadas de fecha para ponerlas como vencidas.
+                                            $queryReservasVencidas = "UPDATE reserva SET estado = 'Vencida' WHERE (finicio < CURDATE())";
+                                            mysqli_query($conexion, $queryReservasVencidas);
+                                            //Ltsta todas las reservas que están en espera de ser aceptadas o rechazadas
+                                            $queryReservasEnEspera = "SELECT * FROM reserva NATURAL JOIN usuario WHERE ((id_couch = '".$_GET["id"]."') AND (estado = 'En espera'))";
+                                            $resultadoReservasEnEspera = mysqli_query($conexion, $queryReservasEnEspera);
+                                            ?>
+                                            <form name="formularioReservasEnEspera" action="consultas/aceptar_rechazar_reserva.php" method="POST">
+                                                <?php    while ($rowReservas = mysqli_fetch_array($resultadoReservasEnEspera)) {
+                                                    ?>
+
+
+                                                    <input type="hidden" name="idCouch" class="form-control" id="idCouch" value="<?php echo($_GET["id"])?>">
+
+
+                                                    <div class="form-group" id="">
                             <?php  echo("Solicitud de ".$rowReservas["nombre"]." ".$rowReservas["apellido"]." del ".$rowReservas["finicio"]." al ".$rowReservas["ffin"]);?>
                             <button type="submit" class="btn btn-sm btn-primary glyphicon glyphicon-ok" name = "aceptar" id="aceptar.<?php  echo($rowReservas["id_reserva"]); ?>" onclick="return confirm('¿Esta seguro de que desea aceptar la reserva?')" value="<?php  echo($rowReservas["id_reserva"]); ?>" class="btn btn-default"> Aceptar</button>
                             <button type="submit" class="btn btn-sm btn-danger glyphicon glyphicon-remove" name = "rechazar" id = "rechazar.<?php  echo($rowReservas["id_reserva"]); ?>" onclick="return confirm('¿Esta seguro de que desea rechazar la reserva?')" value="<?php  echo($rowReservas["id_reserva"]); ?>" class="btn btn-default"> Rechazar</button>
@@ -183,11 +184,30 @@ if (mysqli_num_rows($resultado) == 1){
             </div>
             <!-- Fin de aceptar o rechazar reservas -->
             <?php }  ?>
-                <a class="btn btn-default" href="index.php">Volver</a>
+            <a class="btn btn-default" href="index.php">Volver</a>
 
-                
+            <!-- Text area para hacer preguntas en caso de que no sea el dueño del Couch -->
+
+            <?php if(!$esDuenio){
+
+             ?>
+
+
+            <form name="form-preguntas" class="form-horizontal" method="GET" onsubmit="altaPregunta.php">
+                <div class="form-group">
+                    <label class="control-label" for="preguntaCouch">Haga su pregunta al dueño: </label>
+                    <textarea class="form-control" rows="5" cols="50" maxlength="500" name="preguntaCouch" id="preguntaCouch" required></textarea>
+                </div>
+                <div class="form-group">
+                    <button type="submit" name="submit" class="btn btn-default">Preguntar</button>
+                </div>
+                <input type="hidden" name="idCouch" value=<?php echo($_GET['id']); ?>>
+                <input type="hidden" name="idUsuario" value=<?php echo($_SESSION['id_usuario']); ?>>
+            </form>
+
+            <?php } ?>
                  
-                
+            <!-- Lista de preguntas y respuestas -->
 
 
 
