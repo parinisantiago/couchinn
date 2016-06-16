2e0a0b922406c9dbe2c4358c0a3dd155960d7539
parinisantiago <parinisantiago@gmail.com>
2016-06-16 10:55:15 -0300

    agrega puntajes a la bd, falta cambiar estado de la reserva

--- consultas/alta_puntajeCouch.php
+++ consultas/alta_puntajeCouch.php
@@ -1,3 +1,26 @@
 <?php
+    if(isset($_GET['submit'])) {
+        include("../conectarBD.php");
+        $puntaje= $_GET['puntReser'];
+        $contenido= $_GET['puntReserContenido'];
+        $idCouch= $_GET['id_couch'];
+        $idReserva= $_GET['id_reserva'];
+        $idUsuario= $_GET['id_usuario'];
+        
+        //agrega el puntaje a la bd
 
-    var_dump($_GET);
+        if($contenido == ""){
+
+            $queryPregunta = "INSERT INTO puntoscouch(id_usuario, id_couch, puntaje) VALUES ('" . $idUsuario . "', '" . $idCouch . "', '" . $puntaje . "')";
+
+        } else {
+
+            $queryPregunta = "INSERT INTO puntoscouch(id_usuario, id_couch, comentario, puntaje) VALUES (id_usuario='" . $idUsuario . "', id_couch='" . $idCouch . "', comentario='" . $contenido . "', puntaje='" . $puntaje . "')";
+
+        }
+        mysqli_query($conexion, $queryPregunta);
+
+        //cambiar estado de la reserva
+    } else {
+        echo("que intentas hacer pillin?");
+    }
