20faa94e600ee5cc893ed50ac9e186337c13cf85
parinisantiago <parinisantiago@gmail.com>
2016-06-22 21:45:21 -0300

    validacion del lado del servidor

---
+++ consultas/alta_puntajeUsuario.php
@@ -0,0 +1,24 @@
+<?php
+
+if(isset($_GET['submit'])) {
+    include_once("../conectarBD.php");
+
+    $idUsuario=$_GET['id_usuario'];
+    $idReserva=$_GET['id_reserva'];
+    $idCouch=$_GET['id_couch'];
+    $puntaje=$_GET['puntReser'];
+
+    //guarda el puntaje para el usuario
+    $queryPuntaje ="INSERT INTO puntos_usuario(id_usuario, puntuacion) VALUES ($idUsuario, $puntaje)";
+    mysqli_query($conexion, $queryPuntaje);
+
+    //actualiza la reserva
+    $queryReserva= "UPDATE reserva SET id_puntajeUsuario='$idUsuario'";
+    mysqli_query($conexion, $queryReserva);
+
+    header("Location: ../puntuarHuespedes.php?msg=Su opinion ha sido registrada&&class=alert-success");
+
+}else{
+
+    echo("que haces pillin? ;)");
+}
