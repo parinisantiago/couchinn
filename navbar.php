0e5b92cf93cf1c1916469d7f8fc81b90336e2d96
parinisantiago <parinisantiago@gmail.com>
2016-07-05 08:34:58 -0300

    agregue link al verPerfil.php

--- navbar.php
+++ navbar.php
@@ -104,7 +104,7 @@ if( isset($_SESSION['sesion_usuario'])){
                     <li class="dropdown" >
                         <a href = "#" class="dropdown-toggle" data-toggle= "dropdown" role = "button" aria-haspopup = "true" aria-expanded = "false" ><?php if($esPremium) { echo("<i class='glyphicon glyphicon-star-empty'></i>");} ?> <?php echo($_SESSION["nombre_completo"])?> <span class="caret" ></span ></a >
                         <ul class="dropdown-menu" >
-                            <li ><a href = "#" > Ver perfil </a ></li >
+                            <li ><a href = "verPerfil.php" > Ver perfil </a ></li >
                             <li ><a href = "user_modForm.php" > Modificar perfil </a ></li >
                         </ul >
                     </li >
