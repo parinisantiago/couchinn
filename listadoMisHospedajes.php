1c2d54b4b2bc96581ee78f24a451ce4fd313814f
parinisantiago <parinisantiago@gmail.com>
2016-07-14 11:32:49 -0300

    muestra si el couch esta publicado, despublicado o eliminado

--- listadoMisHospedajes.php
+++ listadoMisHospedajes.php
@@ -26,7 +26,7 @@ include("navbar.php");
     <h1 class="text-center"> Couchs donde me aloje </h1>
     <?php
     include("conectarBD.php");
-    $queryHospedajes = "SELECT nombre_tipo, couch.id_usuario, titulo, descripcion,capacidad, couch.id_couch, ubicacion, direccion,  DATE_FORMAT(finicio, '%d-%m-%y') AS finicio, DATE_FORMAT(ffin,'%d-%m-%y') AS ffin FROM reserva INNER JOIN couch ON (reserva.id_couch = couch.id_couch) INNER JOIN tipo ON (couch.id_tipo = tipo.id_tipo) WHERE estado='finalizada' AND reserva.id_usuario='" . $_SESSION['id_usuario'] . "' ";
+    $queryHospedajes = "SELECT couch.despublicado, eliminado_couch, nombre_tipo, couch.id_usuario, titulo, descripcion,capacidad, couch.id_couch, ubicacion, direccion,  DATE_FORMAT(finicio, '%d-%m-%y') AS finicio, DATE_FORMAT(ffin,'%d-%m-%y') AS ffin FROM reserva INNER JOIN couch ON (reserva.id_couch = couch.id_couch) INNER JOIN tipo ON (couch.id_tipo = tipo.id_tipo) WHERE estado='finalizada' AND reserva.id_usuario='" . $_SESSION['id_usuario'] . "' ";
     $consultaHospedajes = mysqli_query($conexion, $queryHospedajes);
 
     if (mysqli_num_rows($consultaHospedajes) == 0) {
@@ -41,7 +41,20 @@ include("navbar.php");
         <div class="list-group">
 
 
-            <?php   while ($hospedaje = mysqli_fetch_array($consultaHospedajes)) { ?>
+            <?php   while ($hospedaje = mysqli_fetch_array($consultaHospedajes)) {
+
+                if ($hospedaje["despublicado"] != 0){
+                    $hospedaje["titulo"] = $hospedaje["titulo"]." <font color='yellow'>(DESPUBLICADO)</font>";
+                }
+                else if ($hospedaje['eliminado_couch'] != 0)
+                {
+                    $hospedaje["titulo"] = $hospedaje["titulo"]." <font color='red'>(ELIMINADO)</font>";
+                } else {
+                    $hospedaje["titulo"] = $hospedaje["titulo"]." <font color='green'>(PUBLICADO)</font>";
+                }
+
+
+                ?>
                 <a class=list-group-item href="detalle_couch.php?id=<?php echo($hospedaje['id_couch']) ?>" class="list-group-item">
 
                     <div class="row" style="padding-left:10px">
@@ -70,7 +83,15 @@ include("navbar.php");
                             } ?>
                         </div>
                         <div class="col-md-6" style="padding-left: 50px"><h4 class="list-group-item-heading"><?php echo($hospedaje['titulo']); ?></h4>
-                            <p class="list-group-item-text"><?php echo ("<strong>Descripción: </strong>".$hospedaje['descripcion']."<br> <strong>Ubicación: </strong>".$hospedaje['ubicacion']."<br> <strong>Dirección: </strong>".$hospedaje['direccion']."<br> <strong>Capacidad: </strong> para ".$hospedaje['capacidad']." personas. <br> <strong>Tipo: </strong>".$hospedaje['nombre_tipo']). "<br><strong> Inicio estadia: </strong>".$hospedaje['finicio']."<br><strong>Fin estadia: </strong>". $hospedaje['ffin']; ?> </p>
+                            <p class="list-group-item-text">
+                                <strong>Descripción: </strong><?php echo($hospedaje['descripcion']);?><br>
+                                 <strong>Ubicación: </strong><?php echo($hospedaje['ubicacion']);?><br>
+                                  <strong>Dirección: </strong><?php echo($hospedaje['direccion']) ?><br>
+                                  <strong>Capacidad: </strong> para <?php echo($hospedaje['capacidad']); ?> personas <br>
+                                  <strong>Tipo: </strong><?php echo($hospedaje['nombre_tipo']);?><br>
+                                  <strong> Inicio estadia: </strong><?php echo($hospedaje['finicio']);?><br>
+                                  <strong>Fin estadia: </strong><?php echo($hospedaje['ffin']);?>
+                            </p>
                         </div>
 
                     </div>
